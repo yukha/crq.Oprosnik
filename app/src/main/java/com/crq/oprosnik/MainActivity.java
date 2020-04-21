09bf81e79fd43fea5812235e65d9667dfc71d17c
jurij <khartskhaev@gmail.com>
2020-04-22 00:29:32 +0200

    IMEI

--- app/src/main/java/com/crq/oprosnik/MainActivity.java
+++ app/src/main/java/com/crq/oprosnik/MainActivity.java
@@ -1,5 +1,6 @@
 package com.crq.oprosnik;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Notification;
 import android.app.NotificationChannel;
@@ -7,12 +8,14 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.http.SslError;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings.Secure;
+import android.telephony.TelephonyManager;
 import android.webkit.CookieManager;
 import android.webkit.JavascriptInterface;
 import android.webkit.SslErrorHandler;
@@ -24,6 +27,7 @@ import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 
 import java.util.Objects;
@@ -54,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
         CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
         CookieManager.getInstance().setAcceptFileSchemeCookies(true);
 
-        webView.setWebViewClient(new WebViewClient(){
+        webView.setWebViewClient(new WebViewClient() {
 
             @Override
             public void onReceivedHttpError(WebView view,
@@ -68,7 +72,19 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        @SuppressLint("HardwareIds") String android_id = Secure.getString( getApplicationContext().getContentResolver(), Secure.ANDROID_ID);
+        // @SuppressLint("HardwareIds") String android_id = Secure.getString( getApplicationContext().getContentResolver(), Secure.ANDROID_ID);
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
+        String android_id = telephonyManager.getDeviceId();
 
         url = "https://kassa.norana.ru/survey/get?deviceId=" + android_id;
         // url = "https://default.norana.ru/?deviceId=" + android_id;
